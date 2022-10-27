d3ab307dcd8780c06699ad5c4eb67718857f67d2
DOGUKAN ATALAY <dogukanatalay46@gmail.com>
2022-10-27 13:27:06 +0300

    pointer ops added

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+// import "fmt"
+
+// func main() {
+//   i,j := 42, 2701
+//   fmt.Println(i,j)
+//   fmt.Println(&i, &j)
+//   // read the ampersand(&) as "adress of" 
+
+//   p := &i
+//   fmt.Println(p)
+//   fmt.Println(*p)
+//   // *p named "dereferencing"
+//   // pointer allows us to have access to the variable
+//   // without copying it to multiple places and this is
+//   // more efficient than copying the variable
+//   *p = 21
+//   println(i)
+
+//   p = &j
+//   *p = *p / 37
+//   fmt.Println(j)
+// }
+
+
---
+++ second.go
@@ -0,0 +1,23 @@
+package main
+
+// import "fmt"
+
+// func main() {
+//   a := 4
+//   squareVal(a)
+//   squareValAddress(&a)
+//   fmt.Printf("new value : %v", a)
+// }
+
+// func squareVal(v int) {
+//   v *= v
+//   fmt.Println(&v, v)
+// }
+
+
+// func squareValAddress(p *int) {
+//   *p *= *p
+//   fmt.Println(p, *p)
+// }
+
+ 
